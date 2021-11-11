2b974dfda46e1afd72bd47422caf48111e6f09b9
Ioana Amarande <ioanaa@microsoft.com>
2021-11-11 10:58:39 +0100

    Added code review comments

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -12,7 +12,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  #skip_provider_registration = true
 }
 
 resource "random_id" "elms" {
@@ -28,4 +27,15 @@ module "iotelms" {
   iothub_id                    = var.iothub_id
   iothub_name                  = var.iothub_name
   send_metrics_device_to_cloud = var.send_metrics_device_to_cloud
+}
+
+module "iotedgevm" {
+  source                     = "../modules/iotedgevm"
+  name_identifier            = var.name_identifier
+  random_id                  = lower(random_id.elms.hex)
+  iot_hub_name               = var.iothub_name
+  iot_edge_connection_string = var.edge_device_connection_string
+  resource_group_name        = var.rg_name
+  location                   = var.location
+  vm_user_name               = "edge-elms"
 }
