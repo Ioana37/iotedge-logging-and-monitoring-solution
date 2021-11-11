a6e7ac13f8e577acb569fb65a632c9463bf16199
Ioana Amarande <ioanaa@microsoft.com>
2021-11-11 14:59:43 +0100

    iotedge VM module

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -28,3 +28,14 @@ module "iotelms" {
   iothub_name                  = var.iothub_name
   send_metrics_device_to_cloud = var.send_metrics_device_to_cloud
 }
+
+module "iotedgevm" {
+  source                     = "../modules/iotedgevm"
+  name_identifier            = var.name_identifier
+  random_id                  = lower(random_id.elms.hex)
+  iot_hub_name               = var.iothub_name
+  iot_edge_connection_string = var.edge_device_connection_string
+  resource_group_name        = var.rg_name
+  location                   = var.location
+  vm_user_name               = "edge-elms"
+}
